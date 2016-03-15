ddc439ae574d74acee3d0fff182e8a47df534dbc
Luke Cusack <cusack.luke@gmail.com>
2016-03-15 17:18:48 +1100

    week03: updated bankaccount

--- week03/src/week03/BankAccount.java
+++ week03/src/week03/BankAccount.java
@@ -2,9 +2,32 @@ package week03;
 
 public class BankAccount {
 	private int currentBalance;
+	private int amountToday;
+	private Calendar lastTransaction;
+	
+	private final int WITHDRAWAL_LIMIT;
 	
 	public BankAccount() {
-		this.currentBalance = 500;
+		this.currentBalance = 0;
+		amountToday = 0;
+		WITHDRAWAL_LIMIT = 800;
+	}
+	
+	/**
+	 * @precondition amount will always be positive
+	 * @postcondition balance will always be >=0
+	 * @postcondition balance will decrease by amount if under withdrawal limit and account balance
+	 * @param amount
+	 * @throws Exception
+	 */
+	public void withdraw(int amount) {
+		if (amountToday + amount > WITHDRAWAL_LIMIT) {
+			throw new Exception("Amount exceeds daily limit");
+		} else if (amount > accountBalance) {
+			throw new Exception("Amount > Balance");
+		}
+		currentBalance = currentBalance - amount;
+		amountToday += amount;
 	}
 	
 	public int getCurrentBalance() {
